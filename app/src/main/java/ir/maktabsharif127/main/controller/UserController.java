f33d56fbde88707c250229e17ab63cc4d54ba93b
maxixking66 <maxixking66@gamil.com>
2025-06-12 11:55:48 +0330

    add example for request param and path vai

--- app/src/main/java/ir/maktabsharif127/main/controller/UserController.java
+++ app/src/main/java/ir/maktabsharif127/main/controller/UserController.java
@@ -2,9 +2,7 @@ package ir.maktabsharif127.main.controller;
 
 import ir.maktabsharif127.main.dto.UserBriefDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,21 +11,8 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-
-    //    @GetMapping("/get-all-info")
-//    @GetMapping("/{id}/orders")
-//    @GetMapping("/{id}/orders/{orderId}")
-//    @GetMapping("/{id}/addresses")
-//    @GetMapping("/{id}/wallet")
     @GetMapping
     public ResponseEntity<List<UserBriefDTO>> getAllUsers() {
-//        return ResponseEntity.ok(
-//                List.of(
-//                        new UserBriefDTO(1L, "mohsen", "asgari", null, true, null),
-//                        new UserBriefDTO(2L, "mohsen", "asgari", null, true, Collections.emptyList()),
-//                        new UserBriefDTO(32L, "mohsen", "asgari", new UserBriefDTO(), false, List.of("1", "2"))
-//                )
-//        );
         return ResponseEntity.status(201)
                 .header("my-header", "my-header-value")
                 .body(
@@ -37,6 +22,22 @@ public class UserController {
                                 new UserBriefDTO(32L, "mohsen", "asgari", new UserBriefDTO(), false, List.of("1", "2"))
                         )
                 );
+    }
+
+    @GetMapping("/by-param")
+//    requestParam, query string, Query Params -> /users?id=5&username=mohsen&
+    public ResponseEntity<UserBriefDTO> getUserById(@RequestParam Long id) {
+        return ResponseEntity.ok(
+                new UserBriefDTO(id, "mohsen", "asgari", null, true, null)
+        );
+    }
+
+    @GetMapping("/by-path-variable/{id}")
+//    /users/5/orders
+    public ResponseEntity<UserBriefDTO> getUserByIdAndPath(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                new UserBriefDTO(id, "mohsen", "asgari", null, true, null)
+        );
     }
 
 }
