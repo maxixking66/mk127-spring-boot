424525f2e74183a859e6a0abd23b9280925c270c
maxixking66 <maxixking66@gamil.com>
2025-06-13 10:07:04 +0330

    add Validated and groups for nested object

--- app/src/main/java/ir/maktabsharif127/main/dto/UserSaveUpdateRequest.java
+++ app/src/main/java/ir/maktabsharif127/main/dto/UserSaveUpdateRequest.java
@@ -1,5 +1,6 @@
 package ir.maktabsharif127.main.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -23,5 +24,22 @@ public class UserSaveUpdateRequest {
 
     @NotBlank(groups = {ValidationGroup.Save.class, ValidationGroup.Update.class})
     private String password;
+
+    @NotNull(groups = {ValidationGroup.Save.class, ValidationGroup.Update.class})
+    @Valid
+    private Address address;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Address {
+
+        @NotBlank(groups = ValidationGroup.Update.class)
+        private String address;
+
+        @NotBlank(groups = ValidationGroup.Save.class)
+        private String postalCode;
+    }
 
 }
