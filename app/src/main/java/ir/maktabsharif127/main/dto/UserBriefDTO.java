d83bcce013d8c01e107885fa1d469fad8e837898
maxixking66 <maxixking66@gamil.com>
2025-06-13 11:59:35 +0330

    add simple example of jackson annotation

--- app/src/main/java/ir/maktabsharif127/main/dto/UserBriefDTO.java
+++ app/src/main/java/ir/maktabsharif127/main/dto/UserBriefDTO.java
@@ -1,5 +1,7 @@
 package ir.maktabsharif127.main.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,11 @@ import java.util.List;
 public class UserBriefDTO {
 
     private Long id;
+    @JsonProperty(value = "first-name")
     private String firstName;
+    @JsonAlias(value = "last_name")
     private String lastName;
+    @JsonProperty(value = "myDTO")
     private UserBriefDTO dto;
     private Boolean isActive;
     private List<String> numbers;
