34874a79c8d298cab0b8c364124834deb3db3957
maxixking66 <maxixking66@gamil.com>
2025-07-04 10:03:46 +0330

    use JpaRepository and JpaSpecification

--- app/src/main/java/ir/maktabsharif127/main/repository/RoleRepository.java
+++ app/src/main/java/ir/maktabsharif127/main/repository/RoleRepository.java
@@ -1,11 +1,12 @@
 package ir.maktabsharif127.main.repository;
 
 import ir.maktabsharif127.main.domain.Role;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Integer>, JpaSpecificationExecutor<Role> {
 
     boolean existsByName(String name);
 
