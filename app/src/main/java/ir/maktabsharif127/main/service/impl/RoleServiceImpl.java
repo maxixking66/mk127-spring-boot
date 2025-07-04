7b3a51ae45011a295d73b89017bf698556cd3907
maxixking66 <maxixking66@gamil.com>
2025-07-04 09:40:38 +0330

    use @PostConstruct for inserting default role

--- app/src/main/java/ir/maktabsharif127/main/service/impl/RoleServiceImpl.java
+++ app/src/main/java/ir/maktabsharif127/main/service/impl/RoleServiceImpl.java
@@ -3,6 +3,7 @@ package ir.maktabsharif127.main.service.impl;
 import ir.maktabsharif127.main.domain.Role;
 import ir.maktabsharif127.main.repository.RoleRepository;
 import ir.maktabsharif127.main.service.RoleService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,20 @@ import org.springframework.stereotype.Service;
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository repository;
+
+    @PostConstruct
+    public void initDefaultData() {
+        if (repository.count() == 0) {
+            insertRole("ADMIN");
+            insertRole("CUSTOMER");
+        }
+    }
+
+    private void insertRole(String name) {
+        Role role = new Role();
+        role.setName(name);
+        repository.save(role);
+    }
 
     @Override
     public Role save(Role role) {
