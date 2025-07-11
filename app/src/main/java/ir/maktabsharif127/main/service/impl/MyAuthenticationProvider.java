4b5176960641fdea11a92c9c4399c6965a371a08
maxixking66 <maxixking66@gamil.com>
2025-07-11 11:11:46 +0330

    add custom Auth provider

---
+++ app/src/main/java/ir/maktabsharif127/main/service/impl/MyAuthenticationProvider.java
@@ -0,0 +1,53 @@
+package ir.maktabsharif127.main.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = (String) authentication.getPrincipal();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        checkPassword(authentication, userDetails);
+
+        checkIsActive(userDetails);
+
+        return new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+    }
+
+    private void checkIsActive(UserDetails userDetails) {
+        if (!userDetails.isEnabled()) {
+            throw new DisabledException("user is not active");
+        }
+    }
+
+    private void checkPassword(Authentication authentication, UserDetails userDetails) {
+        if (!passwordEncoder.matches((String) authentication.getCredentials(), userDetails.getPassword())) {
+            throw new BadCredentialsException("Bad Credentials");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
